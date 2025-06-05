f250f8bc8096aca2c129547fbe9dd71e3b5e3090
mbayajames <jmuthiora244@gmail.com>
2025-06-05 04:28:24 -0700

    refactor: update CovidDataWidget.vue for improved data rendering and UI enhancements

--- src/components/CovidDataWidget.vue
+++ src/components/CovidDataWidget.vue
@@ -1,40 +1,47 @@
 <template>
-  <div class="widget">
-    <div class="card">
+  <div class="covid-widget">
+    <div class="card" v-if="data">
       <h3>Total Cases</h3>
-      <p class="cases">
-        {{ data.cases ? data.cases.toLocaleString() : "N/A" }}
-      </p>
+      <p>{{ data.cases ? data.cases.toLocaleString() : "N/A" }}</p>
     </div>
-    <div class="card">
-      <h3>Total Deaths</h3>
-      <p class="deaths">
-        {{ data.deaths ? data.deaths.toLocaleString() : "N/A" }}
-      </p>
-    </div>
-    <div class="card">
-      <h3>Total Recovered</h3>
-      <p class="recovered">
-        {{ data.recovered ? data.recovered.toLocaleString() : "N/A" }}
-      </p>
-    </div>
-    <div class="card">
+    <div class="card" v-if="data">
       <h3>Active Cases</h3>
-      <p class="active">
-        {{ data.active ? data.active.toLocaleString() : "N/A" }}
-      </p>
+      <p>{{ data.active ? data.active.toLocaleString() : "N/A" }}</p>
     </div>
+    <div class="card" v-if="data">
+      <h3>Recovered</h3>
+      <p>{{ data.recovered ? data.recovered.toLocaleString() : "N/A" }}</p>
+    </div>
+    <div class="card" v-if="data">
+      <h3>Deaths</h3>
+      <p>{{ data.deaths ? data.deaths.toLocaleString() : "N/A" }}</p>
+    </div>
+    <div class="loading" v-if="loading">Loading...</div>
+    <div class="error" v-if="error">{{ error }}</div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "CovidDataWidget",
-  props: {
-    data: {
-      type: Object,
-      required: true,
-    },
+  data() {
+    return {
+      data: null,
+      loading: true,
+      error: null,
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get("https://disease.sh/v3/covid-19/all");
+      this.data = response.data;
+      this.loading = false;
+    } catch (err) {
+      this.error = "Error fetching data: " + err.message;
+      this.loading = false;
+    }
   },
 };
 </script>
